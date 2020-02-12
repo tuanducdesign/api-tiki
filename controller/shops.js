740b688b84eb9cf5624cad648a03d63c37b9c459
phuong <phuongminh2303@gmail.com>
2020-02-12 22:10:27 +0700

    Add select, sort, pagination queries to GET all shops route

--- controller/shops.js
+++ controller/shops.js
@@ -6,10 +6,65 @@ const asyncHandler = require('../middleware/async');
 // @route   GET /api/v1/shops
 // @access  Public
 const getShops = asyncHandler(async (req, res, next) => {
-  const shops = await Shop.find();
+  const reqQuery = { ...req.query };
+
+  // Select only field from query
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+  // Remove removeFields from reqQuery
+  removeFields.forEach(item => delete reqQuery[item]);
+
+  let queryString = JSON.stringify(reqQuery);
+
+  // Add $ to query string --> it become mongodb query
+  queryString = queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  let query = Shop.find(JSON.parse(queryString));
+
+  // Select query
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+  // Sorting query
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    // Default sorting by createdAt
+    query.sort('-createdAt');
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Shop.countDocuments();
+
+  query.skip(startIndex).limit(limit);
+
+  // Execute the query
+  const shops = await query;
+
+  // Paginating the result
+  const pagination = {};
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    };
+  }
+
   res.status(200).json({
     success: true,
     total: shops.length,
+    pagination: pagination,
     data: shops
   });
 });
@@ -64,7 +119,7 @@ const updateShop = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc    Update shop
+// @desc    Delete shop
 // @route   DELETE /api/v1/shops/:id
 // @access  Private
 const deleteShop = asyncHandler(async (req, res, next) => {
