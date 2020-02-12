a6e0db237b9b19098f713399ddffb93a9d16a0f8
phuong <phuongminh2303@gmail.com>
2020-02-12 15:12:57 +0700

    Connect to mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env variables
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -16,8 +20,16 @@ app.use('/api/v1/shops', shops);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
   );
+});
+
+// Handle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  // Close server and exit
+  server.close(() => process.exit(1));
 });
